79c76309e93d674088074ceca2de2acb52f54e57
tanita plaengprawat <tanitaplaeng@gmail.com>
2020-05-06 22:46:40 -0400

    updated functions

---
+++ script.js
@@ -0,0 +1,27 @@
+function getAreaOfCircle(radius) {
+    let area = Math.PI * (radius * radius);
+    return area;
+}
+
+console.log(getAreaOfCircle(6));
+
+function getCircumferenceOfCircle(radius) { 
+    let circumference = Math.PI * radius * 2; 
+    return circumference;
+}
+
+console.log(getCircumferenceOfCircle(12));
+
+function getAreaOfSquare(side) { 
+    let area = side * side;
+    return area;
+}
+
+console.log(getAreaOfSquare(4));
+
+function getAreaOfTriangle(base, height) { 
+    let area = (base * height) / 2;
+    return area;  
+}
+
+console.log(getAreaOfTriangle(3, 15));
